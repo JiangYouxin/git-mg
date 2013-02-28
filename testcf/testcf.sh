98a0fe0f15154b7a6f56382b756a3a6cde396f49
Jiang Youxin <jiangyouxin9@gmail.com>
2013-02-28 18:19:47 +0800

    add return value support

--- testcf/testcf.sh
+++ testcf/testcf.sh
@@ -3,17 +3,25 @@
 for TESTFILE in *.test; do
     RESULTFILE="$TESTFILE.result"
     RESULTFILEG="$TESTFILE.result.g"
+    RETFILE="$TESTFILE.ret"
+    RETFILEG="$TESTFILE.ret.g"
     TEMPFILE="$TESTFILE.temp"
     TEMPFILEG="$TESTFILE.temp.g"
     cp -f $TESTFILE $TEMPFILE
     cp -f $TESTFILE $TEMPFILEG
     echo "test case $TESTFILE..."
     git cf $TEMPFILE
+    if [ $? -ne `cat $RETFILE` ]; then
+        echo "ERROR: return value $RETFILE"
+    fi
     diff -q $RESULTFILE $TEMPFILE
     if [ $? -eq 0 ]; then
         rm $TEMPFILE
     fi  
     git cf -g $TEMPFILEG
+    if [ $? -ne `cat $RETFILEG` ]; then
+        echo "ERROR: return value $RETFILEG"
+    fi
     diff -q $RESULTFILEG $TEMPFILEG
     if [ $? -eq 0 ]; then
         rm $TEMPFILEG
